057a55f9ae14f9a9940510bca353eb529e20a768
Juan Zamora <jzamora_24@alu.uabcs.mx>
2026-02-10 18:29:08 -0700

    Nuevos campos de usuario y contrasena al login

--- ProyectoFinalProgramacion3/src/views/LoginView.java
+++ ProyectoFinalProgramacion3/src/views/LoginView.java
@@ -3,6 +3,7 @@ package views;
 import java.awt.Color;
 import java.awt.Font;
 
+import javax.swing.JPasswordField;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -18,11 +19,11 @@ public class LoginView extends JPanel{
 		btnConfimar.setFont(new Font("Times New Roman", Font.PLAIN, 23));
 		btnConfimar.setBackground(new Color(48, 60, 26));
 		btnConfimar.setForeground(Color.WHITE);
-		btnConfimar.setBounds(95, 281, 316, 40);
+		btnConfimar.setBounds(95,320, 316, 40);
 		add(btnConfimar);
 		
 		
-		JTextField txtNombre = new JTextField();
+		/*JTextField txtNombre = new JTextField();
 		txtNombre.setBounds(172, 99, 96, 18);
 		add(txtNombre);
 		txtNombre.setColumns(10);
@@ -30,7 +31,31 @@ public class LoginView extends JPanel{
 		JTextField txtEmail = new JTextField();
 		txtEmail.setBounds(172, 146, 96, 18);
 		add(txtEmail);
-		txtEmail.setColumns(10);
+		txtEmail.setColumns(10);*/
+		
+		//USUARIO
+		JLabel labelUsuario = new JLabel("USUARIO");
+		labelUsuario.setForeground(Color.WHITE);
+		labelUsuario.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+		labelUsuario.setBounds(95, 190, 120, 25);
+		add(labelUsuario);
+
+		JTextField txtUsuario = new JTextField();
+		txtUsuario.setBounds(95, 215, 316, 25);
+		add(txtUsuario);
+
+		// CONTRASENA
+		JLabel labelContrasena = new JLabel("CONTRASENA");
+		labelContrasena.setForeground(Color.WHITE);
+		labelContrasena.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+		labelContrasena.setBounds(95, 245, 140, 25);
+		add(labelContrasena);
+
+		JPasswordField txtContrasena = new JPasswordField();
+		txtContrasena.setBounds(95, 270, 316, 25);
+		add(txtContrasena);
+		
+		
 		
 		JLabel lblIniciarSesion = new JLabel("Iniciar Sesion");
 		lblIniciarSesion.setFont(new Font("Times New Roman", Font.PLAIN, 40));
